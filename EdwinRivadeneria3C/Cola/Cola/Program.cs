2a8649093cd1771dcb9b7144d18503818dfce5b6
Edwin Rivadeneira <76056809+savatkd@users.noreply.github.com>
2021-06-21 23:19:50 -0500

    Proyect

---
+++ EdwinRivadeneria3C/Cola/Cola/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdwinRivadeneira3C
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            cola c = new cola();
+            c.encolar();
+            c.encolar();
+            c.encolar();
+            c.encolar();
+            c.encolar();
+            Console.WriteLine("\n Cola ingresada\n");
+            c.DesplegarCola();
+            Console.ReadLine();
+
+        }
+    }
+    //ESTA ES LA ESTRUCTURA DEL NODO
+    //de modo que siempre la usaremos asi
+    class Nodo
+    {
+
+        private int dato;
+        private Nodo siguiente;
+
+        public int Dato
+        {
+            get { return dato; }
+            set { dato = value; }
+        }
+        public Nodo Siguiente
+        {
+            get { return siguiente; }
+            set { siguiente = value; }
+        }
+    }
+    class cola
+    {
+        //instanciamos el nodo ,NO LA COLA 
+        //de esta forma hemos llamado al nodo
+        private Nodo Primero = new Nodo();
+        private Nodo Ultimo = new Nodo();
+
+        //vamos a inicializar la cola utilizando null
+        public cola()
+        {
+            Primero = null;
+            Ultimo = null;
+        }
+        //realizar el metodo para encolar
+        public void encolar()
+        {
+            //insertar un nuevo nodo en la cola
+            Nodo nuevo = new Nodo();
+            Console.WriteLine("ingrese el dato que contendra el nuevo nodo");
+            nuevo.Dato = int.Parse(Console.ReadLine());
+
+            if (Primero == null)
+            {
+                Primero = nuevo;
+                Primero.Siguiente = null;
+                Ultimo = nuevo;
+            }
+            else
+            {
+                Ultimo.Siguiente = nuevo;
+                nuevo.Siguiente = null;
+                Ultimo = nuevo;
+
+            }
+            Console.WriteLine("nodo ingresa correctamente");
+
+        }
+        //metodo para imprimir la cola respectiva
+        public void DesplegarCola()
+        {
+            //vamos a crear un nodo que recorra toda la cola para que
+            //pueda imprimir los elementos
+            Nodo actual = new Nodo();
+            actual = Primero;
+            if (Primero != null)
+            {
+
+                while (actual != null)
+                {
+
+                    Console.WriteLine(" " + actual.Dato);
+                    actual = actual.Siguiente;//la linea recorrera todo
+
+
+                }
+                //quiere decir que la cola si esta llena
+
+            }
+            else
+            {
+                Console.WriteLine("LA COLA ESTA VACIA");
+            }
+
+
+        }
+
+    }
+
+}
---
+++ EdwinRivadeneria3C/EdwinRivadeneria3C/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace EdwinRivadeneira3C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack miPila = new Stack();
+            int opcion; //opcion del menu 
+            do
+            {
+                Console.Clear();    //Se limpia la consola
+                opcion = menu();    //Se muestra el menu y espera la opción
+                switch (opcion)
+                {
+                    case 1:
+                        agregar(ref miPila);
+                        break;
+                    case 2:
+                        eliminar(ref miPila);
+                        break;
+                    case 3:
+                        limpiar(ref miPila);
+                        break;
+                    case 4:
+                        imprimir(miPila);
+                        break;
+                    case 5: break; //salir
+                    default:
+                        mensaje("ERROR: la opción no es valida. Intente de nuevo.");
+                        break;
+                }
+            }
+            while (opcion != 5);
+            mensaje("El programa a finalizado.");
+        }
+
+        /* Se añade un nuevo elemento a la pila */
+        static void agregar(ref Stack pila)
+        {
+            Console.Write("\nIngresar el valor: ");
+            try
+            {
+                int valor = Convert.ToInt32(Console.ReadLine());
+                if (valor > 99 || valor <= 0)
+                {
+                    mensaje("Solo números del 1 al 99");
+                }
+                else
+                {
+                    pila.Push(valor);
+                    imprimir(pila);
+                }
+            }
+            catch
+            {
+                mensaje("Error: Solo números del 1 al 99");
+            }
+        }
+
+        /* Elimina todo los elementos que se encuentran en la pila */
+        static void limpiar(ref Stack pila)
+        {
+            pila.Clear();
+            imprimir(pila);
+        }
+
+        /* Elimina elemento de la pila */
+        static void eliminar(ref Stack pila)
+        {
+            if (pila.Count > 0)
+            {
+                int valor = (int)pila.Pop();
+                mensaje("Elemento " + valor + " eliminado");
+            }
+            else
+            {
+                imprimir(pila);
+            }
+        }
+
+        /* Se muestra el menu y retorna la opción */
+        static int menu()
+        {
+            //Console.Clear();
+            Console.WriteLine("\n       Stack Menu\n");
+            Console.WriteLine(" 1.- Agregar elementos");
+            Console.WriteLine(" 2.- Eliminar elementos");
+            Console.WriteLine(" 3.- Vaciar Pila");
+            Console.WriteLine(" 4.- Ver pila");
+            Console.WriteLine(" 5.- Finalizar el sistema");
+            Console.Write("=> digita una opción de los intens: ");
+            try
+            {
+                int valor = Convert.ToInt32(Console.ReadLine());
+                return valor;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        /* Muestra mensaje del programa al usuario */
+        static void mensaje(String texto)
+        {
+            if (texto.Length > 0)
+            {
+                Console.WriteLine("\n  =========================================================");
+                Console.WriteLine(" => {0}", texto);
+                Console.WriteLine(" =======================================================");
+                Console.WriteLine("\n Presione cualquier tecla para continuar con el sistema");
+                Console.ReadKey();
+            }
+        }
+
+        /* Imprime la pila */
+        static void imprimir(Stack pila)
+        {
+            if (pila.Count > 0)
+            {
+                Console.WriteLine("");
+                foreach (int dato in pila)
+                {
+                    Console.WriteLine(" |  |  |");
+                    if (dato < 10)
+                        Console.WriteLine(" | 0{0}  |", dato);
+                    else
+                        Console.WriteLine(" | {0}  |", dato);
+                    Console.WriteLine(" |_|_|");
+                }
+                Console.WriteLine("\nPresione cualquier tecla para continuar con el sistema");
+                Console.ReadKey();
+            }
+            else
+            {
+                mensaje("La Pila se encuentra vacia");
+            }
+        }
+    }
+}
